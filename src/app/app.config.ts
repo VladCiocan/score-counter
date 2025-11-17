930ee44c0c1f10e9df2c198789c51e34bddbfa11
Vlad Ciocan <vlad.ciocan@urbio-romania.ro>
2025-11-17 14:22:05 +0200

    theme add

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,27 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from '../app/app.routes';
 
-// PrimeNG standalone config (important!)
-import { provideAnimations } from '@angular/platform-browser/animations';
+import Aura from '@primeuix/themes/aura';
 import { MessageService } from 'primeng/api';
-import { ToastModule } from 'primeng/toast'; // ✅ Înlocuiește MessagesModule/MessageModule
+import { ToastModule } from 'primeng/toast';
+import {providePrimeNG} from 'primeng/config';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async'; // ✅ Înlocuiește MessagesModule/MessageModule
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimations(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false
+        }
+      }
+    }),
     MessageService,
-    importProvidersFrom(ToastModule) // Toast pentru mesaje (vezi mai jos)
+    importProvidersFrom(ToastModule)
   ]
 };
